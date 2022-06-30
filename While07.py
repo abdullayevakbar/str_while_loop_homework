f8466ab4b506942428955f266f888c348eca5554
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2022-06-30 21:01:59 +0500

    while

--- While07.py
+++ While07.py
@@ -10,6 +10,6 @@ def main(s):
     x = 0
     while i < len(s):
         if s[i] >= 0 and s[i] <= '9' and (s[i]-'0') % 2 == 0:
-            x += 1
+            x = x + 1
         i += 1
     return x
